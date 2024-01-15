c98a99ebc72bd82f60dd23f2cf8a7a22d1db2608
sunghoon826 <sunghoon898@gmail.com>
2024-01-15 15:54:06 +0900

    데이터 페이지로 불러오기

--- Services/TdmsFileService.cs
+++ Services/TdmsFileService.cs
@@ -1,42 +1,46 @@
 ﻿using BlazorServerSyncfusion.Interfaces;
 using BlazorServerSyncfusion.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
+using System.Text.Json;
 
 namespace BlazorServerSyncfusion.Services
 {
-
     public class TdmsFileService : IDatabase<TdmsFile>
     {
         private readonly TdmsFilesContext _context;
+
         public TdmsFileService(TdmsFilesContext context)
         {
-
             _context = context;
-
         }
+
         public async Task<List<TdmsFile>> GetAsync()
         {
-            return await this._context.TdmsFiles.ToListAsync();
+            return await _context.TdmsFiles.ToListAsync();
         }
 
         public TdmsFile GetDetail(int? id)
         {
             if (id == null || _context?.TdmsFiles == null)
             {
-                throw new NullReferenceException();
+                throw new ArgumentNullException(nameof(id));
             }
 
-            var validData = this._context.TdmsFiles.FirstOrDefault(x => x.Id == id);
+            return _context.TdmsFiles.Find(id);
+        }
 
-            if (validData != null)
+        // 바이너리 데이터를 double 배열로 변환하는 메소드
+        public double[] ConvertToDoubleArray(byte[] data)
+        {
+            if (data == null || data.Length % 8 != 0)
+                return Array.Empty<double>();
+
+            double[] result = new double[data.Length / 8];
+            for (int i = 0; i < result.Length; i++)
             {
-                return validData;
+                result[i] = BitConverter.ToDouble(data, i * 8);
             }
-            else
-            {
-                throw new InvalidOperationException();
-            }
+            return result;
         }
     }
 }
--- obj/Debug/net8.0/BlazorServerSyncfusion.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorServerSyncfusion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorServerSyncfusion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e454a223d2ecdd10f887024a45061ea642db39c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorServerSyncfusion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorServerSyncfusion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
