8e454a223d2ecdd10f887024a45061ea642db39c
sunghoon826 <sunghoon898@gmail.com>
2024-01-15 14:07:27 +0900

    Db Context 및 Services, Interface

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using BlazorServerSyncfusion.Components;
+using BlazorServerSyncfusion.Interfaces;
+using BlazorServerSyncfusion.Models;
+using BlazorServerSyncfusion.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<TdmsFilesContext>();
+builder.Services.AddScoped<IDatabase<TdmsFile>, TdmsFileService>();
 
 var app = builder.Build();
 
---
+++ Services/TdmsFileService.cs
@@ -0,0 +1,42 @@
+﻿using BlazorServerSyncfusion.Interfaces;
+using BlazorServerSyncfusion.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BlazorServerSyncfusion.Services
+{
+
+    public class TdmsFileService : IDatabase<TdmsFile>
+    {
+        private readonly TdmsFilesContext _context;
+        public TdmsFileService(TdmsFilesContext context)
+        {
+
+            _context = context;
+
+        }
+        public async Task<List<TdmsFile>> GetAsync()
+        {
+            return await this._context.TdmsFiles.ToListAsync();
+        }
+
+        public TdmsFile GetDetail(int? id)
+        {
+            if (id == null || _context?.TdmsFiles == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            var validData = this._context.TdmsFiles.FirstOrDefault(x => x.Id == id);
+
+            if (validData != null)
+            {
+                return validData;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
