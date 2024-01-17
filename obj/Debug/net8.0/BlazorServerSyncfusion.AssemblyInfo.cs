4ecba296703dae03875bc00818711fb801df17bd
sunghoon826 <sunghoon898@gmail.com>
2024-01-17 13:46:50 +0900

    Chart에 표현

--- obj/Debug/net8.0/BlazorServerSyncfusion.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorServerSyncfusion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorServerSyncfusion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aabf2b002caed49c5661670b319d4ecf221c0291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09540caf247258940e2cf3e3e1db71b3e6cdd594")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorServerSyncfusion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorServerSyncfusion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
